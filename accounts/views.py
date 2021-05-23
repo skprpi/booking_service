916706b494d45df517a64c012bef2c1acb666511
skprpi <fatin.maksim@gmail.com>
2021-05-23 22:49:51 +0300

    Updated functional: created ability to work with data from BD

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
+
 from django.contrib.auth import login, get_user_model
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import UserLoginForm, UserRegisterForm
-from .models import UserRoles, Lesson, PriceDiscipline, Discipline, DisciplineDescription
+from .models import UserRoles, Lesson, DisciplineDescription
 
 User = get_user_model()
 
@@ -30,22 +32,32 @@ def user_login(request):
 
 
 def home(request, pk):
-    lessons = Lesson.objects.filter(student_pk=request.user.id) | Lesson.objects.filter(teacher_pk=request.user.id)
+    lessons = Lesson.objects.filter(
+        student_pk=request.user.id) | Lesson.objects.filter(
+        teacher_pk=request.user.id)
     n = 30
-    user_context = {'pk': request.user.id, 'text': get_object_or_404(User, pk=request.user.id).email, 'lessons': lessons, 'n': range(30)}
+    user_context = {'pk': request.user.id,
+                    'text': get_object_or_404(User, pk=request.user.id).email,
+                    'lessons': lessons, 'n': range(30)}
     if request.user.role != UserRoles.ADMIN and request.user.id != pk:
         return render(request, 'home.html', context=user_context)
-    admin_context = {'pk': pk, 'text': get_object_or_404(User, pk=pk).email, 'lessons': lessons, 'n': range(30)}
+    admin_context = {'pk': pk, 'text': get_object_or_404(User, pk=pk).email,
+                     'lessons': lessons, 'n': range(30)}
     return render(request, 'home.html', context=admin_context)
 
 
-# pk - id у кого смотрю расписание
 def home2(request, teacher_pk):
     teacher = get_object_or_404(User, pk=teacher_pk)
     if teacher.role != UserRoles.TEACHER or request.user.role != UserRoles.STUDENT:
         return
 
-    lessons = Lesson.objects.filter(student_pk=request.user.id) | Lesson.objects.filter(teacher_pk=request.user.id)
+    startdate = datetime.today().replace(minute=0, hour=0, second=0)
+    enddate = startdate + timedelta(days=120)
+
+    lessons = Lesson.objects.filter(student_pk=request.user.id,
+                                    start_datetime__range=[startdate,
+                                                           enddate]) | Lesson.objects.filter(
+        teacher_pk=request.user.id, start_datetime__range=[startdate, enddate])
     disciplines = DisciplineDescription.objects.filter(teacher_pk=teacher_pk)
     time_price = {}
     for el in disciplines:
@@ -54,10 +66,20 @@ def home2(request, teacher_pk):
         time_price[el.name].append(f'{el.duration} min - {el.price}')
     print(time_price)
 
+    days = [[] for _ in range(30)]
+    for el in lessons:
+        time = el.start_datetime
+        time = time.replace(tzinfo=None)
 
-    p = [[40, 41, 42, 43, 56, 57, 58], [63, 64, 65],[53, 54, 55],[]]
+        delta = time - datetime.today().replace(minute=0, hour=0, second=0)
 
+        day_idx = delta.days
+        idx = (time.hour * 60 + time.minute + 1) // 15
+        for i in range((el.duration + 1) // 15):
+            days[day_idx].append(idx + i)
 
+    p = days
+    print(p)
 
     user_context = {
         'pk': request.user.id,
